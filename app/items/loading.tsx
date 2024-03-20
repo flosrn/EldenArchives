a8faceafc64d2ba443e97e2b50d3a62cad71e223
Flosrn <flosrn.dev@gmail.com>
2024-03-20 17:41:47 +0100

    improve ui

--- app/items/loading.tsx
+++ app/items/loading.tsx
@@ -5,7 +5,7 @@ export default function RouteLoading() {
   return (
     <Layout>
       <LayoutContent className="space-y-4">
-        <div className="flex justify-between">
+        <div className="flex flex-col space-y-4 lg:flex-row lg:justify-between">
           <div className="space-y-3">
             <Skeleton className="h-9 w-24" />
             <Skeleton className="h-5 w-36" />
@@ -14,7 +14,7 @@ export default function RouteLoading() {
             <Skeleton className="h-10 w-[195px]" />
           </div>
         </div>
-        <Skeleton className="h-6 w-1/3" />
+        <Skeleton className="h-6 w-[300px]" />
         <div className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
           {Array.from({ length: 9 }).map((_, index) => (
             <Skeleton key={index} className="h-[350px]" />
