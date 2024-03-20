8cd620f357442b75f7e9f84cc9eaac53084fa872
Flosrn <flosrn.dev@gmail.com>
2024-03-20 14:15:41 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    apple: "/apple-icon.png",
   },
   metadataBase: new URL(getServerUrl()),
 };
