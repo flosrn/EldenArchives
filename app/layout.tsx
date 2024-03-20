748570ffe8feeb56769d07581a0bee00353bac1b
Flosrn <flosrn.dev@gmail.com>
2024-03-20 18:03:57 +0100

    add meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
           <PlausibleProvider domain={SiteConfig.domain} />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            content="width=device-width, initial-scale=1.0"
           />
         </head>
         <body
