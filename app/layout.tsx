3302b8cf555bf96f9d8e7d3f67459cec53f4dab1
Flosrn <flosrn.dev@gmail.com>
2024-03-20 18:07:47 +0100

    remove meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,10 +69,6 @@ export default function RootLayout({
       <html lang="en" className="h-full" suppressHydrationWarning>
         <head>
           <PlausibleProvider domain={SiteConfig.domain} />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
         </head>
         <body
           className={cn(
