559f67890c0f0195c44ecba5646d1a5aefb8c2a3
Flosrn <flosrn.dev@gmail.com>
2024-03-20 19:35:14 +0100

    try to fix input zoom on iOS

--- app/items/ItemsSearch.tsx
+++ app/items/ItemsSearch.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -14,34 +13,6 @@ export const ItemsSearch = ({ placeholder }: ItemsSearchProps) => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    const input = inputRef.current;
-    console.log("input : ", input);
-    if (!input) return;
-
-    const handleFocus = () => {
-      const currentFontSize = window.getComputedStyle(input).fontSize;
-      input.style.fontSize = "16px"; // Taille minimale pour éviter le zoom sur iOS
-      input.dataset.originalFontSize = currentFontSize;
-    };
-
-    const handleBlur = () => {
-      const originalFontSize = input.dataset.originalFontSize;
-      if (originalFontSize) {
-        input.style.fontSize = originalFontSize;
-      }
-    };
-
-    input.addEventListener("focus", handleFocus);
-    input.addEventListener("blur", handleBlur);
-
-    return () => {
-      input.removeEventListener("focus", handleFocus);
-      input.removeEventListener("blur", handleBlur);
-    };
-  }, []);
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
@@ -55,10 +26,10 @@ export const ItemsSearch = ({ placeholder }: ItemsSearchProps) => {
 
   return (
     <Input
-      ref={inputRef}
       placeholder={placeholder}
       onChange={(event) => handleSearch(event.target.value)}
       defaultValue={searchParams.get("search")?.toString()}
+      className="text-base"
     />
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,10 @@ export default function RootLayout({
       <html lang="en" className="h-full" suppressHydrationWarning>
         <head>
           <PlausibleProvider domain={SiteConfig.domain} />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
         </head>
         <body
           className={cn(
